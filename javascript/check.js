52bd7997230fdcfc71167943209fb5128dd600d6
shyamunnikrishnan <unnikrishnanshyam@gmail.com>
2020-03-10 10:26:19 +0530

    Add javascript/check.js, javascript/scope.js

---
+++ javascript/check.js
@@ -0,0 +1,7 @@
+let x = 20;
+
+if ((x % 2) == 0) {
+    console.log(x + ' is an even number');
+} else {
+    console.log(x + ' is an odd number');
+}
---
+++ javascript/scope.js
@@ -0,0 +1,14 @@
+var x = 3;
+let y = 11;
+
+{
+    let z = 21;
+    
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+
+console.log(x);
+console.log(y);
+// console.log(z); 
