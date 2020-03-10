f0797832df487b3da060bf82694fd9b50e3adc74
shyamunnikrishnan <unnikrishnanshyam@gmail.com>
2020-03-10 11:29:09 +0530

    Update javascript/reverse.js

--- javascript/reverse.js
+++ javascript/reverse.js
@@ -1,27 +1,3 @@
-// let x = 401;
-
-    // let y = (x % 10) * 100;
-    // x = Math.floor(x / 10);
-    // y = y + (x % 10) * 10;
-    // x = Math.floor(x / 10);
-
-    // y = y + x;
-
-    
-    // let length = x.toString().length;
-    // let y = 0;
-
-    // for(i = 10; i= i * 10; i < x ) {
-    //     y = y + (x % i) * Math.pow(10, (length / (i/10)));        
-    // }
-    
-    // a = (x / i) * 10;
-
-    // y = y + Math.floor(a);
-
-    
-    // console.log(y);
-
 var x = 4321;
 var rev = 0;
 var rem = 0;
